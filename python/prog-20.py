bda8fecbe14688c845cf2f71494c276e80f7cf8d
Gerardo Beltran <contacto@gerardobeltran.mx>
2021-09-18 12:17:59 -0600

    .

---
+++ python/prog-20.py
@@ -0,0 +1,9 @@
+# Tabla de conversion de temperaturas C --> F
+
+print ("Celsius  <----->   Fahrenheit") 
+
+for Celsius in range(1,26):
+     Fahrenheit = Celsius *1.8 + 32
+     print ("%7d  <----->   %.2f" % (Celsius, Fahrenheit)) 
+
+
