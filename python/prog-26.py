25996edd36953e0a46ac1ddb04743f0cc47acc74
gerardobeltran <contacto@gerardobeltran.mx>
2021-09-23 18:50:06 -0600

    .

---
+++ python/prog-26.py
@@ -0,0 +1,11 @@
+cmInicio = int(input("Indicar centimetros inicial: "))
+cmFin = int(input("Indicar centimetros final: "))
+print ("Centimetros      Metros        Yardas        Pulgadas")
+for i in range(cmInicio, cmFin+1):
+    metros = i / 100
+    yardas = i / 91.44
+    pulgadas = i / 2.54
+    print("%11d      %6.2f        %6.2f       %9.2f" % (i, metros, yardas, pulgadas))
+
+
+
