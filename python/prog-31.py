c2a91ff9175bd46466b85158e3cf67b271af00f8
gerardobeltran <contacto@gerardobeltran.mx>
2021-09-24 18:20:08 -0600

    .

---
+++ python/prog-31.py
@@ -0,0 +1,11 @@
+cadena = "ATGCAAATTGTGTGTGCATAATTTATATAGGCTAGAATAGAATCGCTA"
+seg = "GC"
+
+num = cadena.count("GC")
+tam = len(cadena) / 2
+porcentaje = num / tam *100
+print("El porcentaje es %.2f" % porcentaje)
+
+
+
+
